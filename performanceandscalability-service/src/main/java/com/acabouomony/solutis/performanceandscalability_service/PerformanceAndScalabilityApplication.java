7aeead468f8673396692a7e3999fca10964c1e03
Pietra Almeida <costapietra@gmail.com>
2024-10-31 11:17:39 -0300

    feat(performanceandscalability): initial project setup with Spring Boot and Dockerfile configuration

---
+++ performanceandscalability-service/src/main/java/com/acabouomony/solutis/performanceandscalability_service/PerformanceAndScalabilityApplication.java
@@ -0,0 +1,11 @@
+package com.acabouomony.solutis.performanceandscalability_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerformanceAndScalabilityApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PerformanceAndScalabilityApplication.class, args);
+    }
+}
