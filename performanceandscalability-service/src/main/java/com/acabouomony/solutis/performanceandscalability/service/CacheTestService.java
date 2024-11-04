f341ed8cd2e7696a5d5f12e25da5084303f04fc1
Pietra Almeida <costapietra@gmail.com>
2024-11-04 11:02:56 -0300

    feat: add monitoring with Actuator and Redis caching integration
    - Add Spring Boot Actuator for health checks and monitoring endpoints
    - Integrate Redis as a caching layer to enhance performance
    - Implement detailed logging in CacheTestService to track cache usage and performance

--- performanceandscalability-service/src/main/java/com/acabouomony/solutis/performanceandscalability/service/CacheTestService.java
+++ performanceandscalability-service/src/main/java/com/acabouomony/solutis/performanceandscalability/service/CacheTestService.java
@@ -1,15 +1,27 @@
 package com.acabouomony.solutis.performanceandscalability.service;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CacheTestService {
 
+    private static final Logger logger = LoggerFactory.getLogger(CacheTestService.class);
+
     @Cacheable("exampleCache")
     public String getExampleData(String param) {
-        System.out.println("Executando método getExampleData para o parâmetro: " + param);
-        return "Dados processados para " + param;
+        // Log informativo ao iniciar o método
+        logger.info("Iniciando o método getExampleData com parâmetro: {}", param);
+
+        // Processamento simulado de dados
+        String data = "Dados processados para " + param;
+
+        // Log de depuração para verificar o conteúdo dos dados gerados
+        logger.debug("Dados gerados: {}", data);
+
+        return data;
     }
 }
